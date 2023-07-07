8ce93ce927eadbc7dc096219d4d83d56c5c7d3a4
Callum Kloos <callumkloos@gmail.com>
2023-07-07 15:59:28 +0100

    update formatting rules

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  printWidth: 90,
+  useTabs: true,
+  tabWidth: 4,
+  overrides: [
+    {
+      files: ["*.tsx", "*.jsx", "*.html"],
+      options: {
+        useTabs: false,
+        tabWidth: 2,
+      },
+    },
+  ],
+};
